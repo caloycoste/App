726b6d59653442f2aa35eea634da0e33555abd2c
Carlo Coste <cgcoste@iskolarngbayan.pup.edu.ph>
2021-03-18 18:08:59 +0800

    init

--- myApp/Screens/Posts.js
+++ myApp/Screens/Posts.js
@@ -1,67 +1,23 @@
 /* eslint-disable prettier/prettier */
-import React, { useState, useEffect } from 'react'
-import { TouchableOpacity } from 'react-native'
-import { FlatList } from 'react-native'
-import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import React from 'react'
+import { View } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import History from './History'
+import Missions from './Missions'
+import Capsules from './Capsules'
 
 function Posts() {
 
-    const [data, setData] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
-
-    useEffect(() => {
-        fetch('https://api.spacexdata.com/v3/history')
-            .then((response) => response.json()
-                .then((json) => setData(json)))
-        setIsLoading(false)
-    }, [])
+    const tab = createBottomTabNavigator();
 
     return (
-        <View style={styles.container}>
-            {isLoading ? <View><ActivityIndicator size='large' animating /></View> 
-            : <View>
-                <FlatList
-                    data={data}
-                    renderItem={({ item }) => (
-                        <View>
-                            <TouchableOpacity>
-                            <Text style={styles.title}>{item.title}</Text>
-                            </TouchableOpacity>
-                            <Text style={styles.details}>{item.details}</Text>
-                        </View>
-                    )} />
-            </View>}
-        </View>
-
+        <tab.Navigator>
+            <tab.Screen name='History' component={History} />
+            <tab.Screen name='Missions' component={Missions} />
+            <tab.Screen name='Capsules' component={Capsules} />
+        </tab.Navigator>
 
     )
 }
-
-const styles = StyleSheet.create ({
-    container: {
-        flex:1,
-        paddingTop:20,
-        paddingLeft:20,
-    },
-
-    title: {
-        fontSize:15,
-        fontWeight:'bold',
-        marginBottom:10,
-        color:'teal'
-    },
-
-    details: {
-        textAlign:'justify',
-        marginBottom:10,
-        paddingRight:20,
-
-    }
-
-
-}
-
-
-)
 
 export default Posts
