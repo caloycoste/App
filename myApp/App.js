639107688f3f3b776d5faecec54d77e43522001d
Carlo Coste <cgcoste@iskolarngbayan.pup.edu.ph>
2021-03-18 13:21:03 +0800

    init

--- myApp/App.js
+++ myApp/App.js
@@ -6,6 +6,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 import Home from './Screens/Home'
 import About from './Screens/About'
 import SignUp from './Screens/SignUp'
+import Posts from './Screens/Posts'
 
 
 const stack = createStackNavigator();
@@ -18,6 +19,7 @@ function App() {
           <stack.Screen name='Home' component={Home} />
           <stack.Screen name='About' component={About} />
           <stack.Screen name='Sign Up' component={SignUp}/>
+          <stack.Screen name='Posts' component={Posts}/>
         </stack.Navigator>
       </NavigationContainer>
 
