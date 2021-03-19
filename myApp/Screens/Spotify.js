cffb0be193dd378eaedc0a5be0f3484cb6207d78
Carlo Coste <cgcoste@iskolarngbayan.pup.edu.ph>
2021-03-19 14:00:18 +0800

    init

--- myApp/Screens/Spotify.js
+++ myApp/Screens/Spotify.js
@@ -1,15 +1,29 @@
 /* eslint-disable prettier/prettier */
 import React from 'react'
-import {View, Text} from 'react-native'
+import { View, Text, TouchableOpacity, Button } from 'react-native'
+import { Icon } from 'react-native-elements'
 
 function Spotify() {
     return (
         <View>
-            <Text>
-                Spotify
-            </Text>
+            <View>
+                <Icon name='spotify' type='font-awesome' />
+                <Text>Million of Songs.</Text>
+                <Text>Free on Spotify</Text>
+            </View>
+            <View>
+                <TouchableOpacity>
+                    <Text>Sign up free</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Icon name='google' type='font-awesome'/>
+                    <Text>Sign up free</Text>
+                </TouchableOpacity>
+
+            </View>
+
         </View>
-        
+
     )
 }
 
--- myApp/Screens/myApps.js
+++ myApp/Screens/myApps.js
@@ -8,66 +8,66 @@ function myApps({ navigation }) {
 
     return (
         <View style={styles.container}>
-            <View style={styles.optionBar}>
-                <Icon name='facebook' color='#1877f2' size={40}/>
-                <TouchableOpacity onPress={()=> {
-                    navigation.navigate('Facebook')}}>
-                    <Text style={styles.label}>Facebook</Text></TouchableOpacity>
-            </View>
+            <TouchableOpacity style={styles.optionBar} onPress={() => {
+                navigation.navigate('Facebook')
+            }}>
+                <Icon name='facebook' color='#1877f2' size={40} />
+                <Text style={styles.label}>Facebook</Text>
+            </TouchableOpacity>
 
-            <View style={styles.optionBar}>
+            <TouchableOpacity style={styles.optionBar} onPress={() => {
+                navigation.navigate('Twitter')
+            }}>
                 <Icon name='twitter' type='font-awesome' color='#1da1f2' size={40} />
-                <TouchableOpacity onPress={()=> {
-                    navigation.navigate('Twitter')}}>
-                    <Text style={styles.label}>Twitter</Text></TouchableOpacity>
-            </View>
+                <Text style={styles.label}>Twitter</Text>
+            </TouchableOpacity>
 
-            <View style={styles.optionBar}>
-                <Icon name='instagram' type='font-awesome' color='#c32aa3' size={40}/>
-                <TouchableOpacity onPress={()=> {
-                    navigation.navigate('Instagram')}}>
-                    <Text style={styles.label}>Instagram</Text></TouchableOpacity>
-            </View>
+            <TouchableOpacity style={styles.optionBar} onPress={() => {
+                navigation.navigate('Instagram')
+            }}>
+                <Icon name='instagram' type='font-awesome' color='#c32aa3' size={40} />
+                <Text style={styles.label}>Instagram</Text>
+            </TouchableOpacity>
 
-            <View style={styles.optionBar}>
-                <Icon name='spotify' type='font-awesome' color='#1ed760' size={40}/>
-                <TouchableOpacity onPress={()=> {
-                    navigation.navigate('Spotify')}}>
-                    <Text style={styles.label}>Spotify</Text></TouchableOpacity>
-            </View>
+            <TouchableOpacity style={styles.optionBar} onPress={() => {
+                navigation.navigate('Spotify')
+            }}>
+                <Icon name='spotify' type='font-awesome' color='#1ed760' size={40} />
+                <Text style={styles.label}>Spotify</Text>
+            </TouchableOpacity>
 
-            <View style={styles.optionBar}>
+            <TouchableOpacity style={styles.optionBar} onPress={() => {
+                navigation.navigate('SpaceX')
+            }}>
                 <Icon name='rocket' type='font-awesome' color='#d32323' size={40} />
-                <TouchableOpacity onPress={()=> {
-                    navigation.navigate('SpaceX')}}>
-                    <Text style={styles.label}>SpaceX</Text></TouchableOpacity>
-            </View>
+                <Text style={styles.label}>SpaceX</Text>
+            </TouchableOpacity>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex:1,
-        justifyContent:'center',
+        flex: 1,
+        justifyContent: 'center',
     },
 
     optionBar: {
-        display:'flex',
-        alignItems:'center',
-        flexDirection:'row',
-        alignSelf:'center',
-        marginVertical:10,
-        backgroundColor:'#e1e1e1',
-        width:'70%',
-        padding:20,
-        borderRadius:15,
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'row',
+        alignSelf: 'center',
+        marginVertical: 10,
+        backgroundColor: '#e1e1e1',
+        width: '70%',
+        padding: 20,
+        borderRadius: 15,
     },
 
     label: {
-        marginHorizontal:20,
-        fontSize:18,
-        fontWeight:'bold'
+        marginHorizontal: 20,
+        fontSize: 18,
+        fontWeight: 'bold'
     }
 
 })
