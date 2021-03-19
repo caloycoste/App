38a741f64e7c3501c8d9bf05ae633b398864c94e
Carlo Coste <cgcoste@iskolarngbayan.pup.edu.ph>
2021-03-19 12:36:25 +0800

    init

---
+++ myApp/Components/Messages.js
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import React from 'react'
+import {View, Text} from 'react-native'
+
+function Messages() {
+    return (
+        <View>
+            <Text>
+            
+
+            </Text>
+        </View>
+    )
+}
+
+export default Messages
---
+++ myApp/Screens/myApps.js
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+import React from 'react'
+import { View, Text } from 'react-native'
+import { Icon } from 'react-native-elements'
+
+function myApps({ navigation }) {
+
+
+    return (
+        <View>
+            <View>
+                <Icon name='facebook' color='#1877f2' />
+                <Text>Facebook</Text>
+            </View>
+
+        </View>
+
+
+
+    )
+}
+
+export default myApps
